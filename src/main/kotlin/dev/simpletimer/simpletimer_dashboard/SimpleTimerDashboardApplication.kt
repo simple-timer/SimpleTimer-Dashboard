644037912b9fead827ad5b650999425bbe03dba0
mqrimo <marimoturf@gmail.com>
2024-02-08 20:46:08 +0900

    initial commit.

---
+++ src/main/kotlin/dev/simpletimer/simpletimer_dashboard/SimpleTimerDashboardApplication.kt
@@ -0,0 +1,11 @@
+package dev.simpletimer.simpletimer_dashboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SimpleTimerDashboardApplication
+
+fun main(args: Array<String>) {
+    runApplication<SimpleTimerDashboardApplication>(*args)
+}
---
+++ src/test/kotlin/dev/simpletimer/simpletimer_dashboard/SimpleTimerDashboardApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.simpletimer.simpletimer_dashboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SimpleTimerDashboardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
